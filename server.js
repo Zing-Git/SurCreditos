168b127b5a249d6709d42271b4d70b44a67f273e
Jorge Mendoza <jormendoza@gmail.com>
2018-07-20 13:41:03 -0300

    agregando nombre app de keroku2

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/<name-of-app>'));
-app.use(express.static(__dirname + '/dist/surcreditosapp'));
+// app.use(express.static(__dirname + '/dist/surcreditosapp'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/surcreditosapp'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

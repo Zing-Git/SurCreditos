f35cb1be5ec35200741af8691beaa4f52137b6aa
Jorge Mendoza <jormendoza@gmail.com>
2018-07-24 18:52:15 -0300

    otra url

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/<name-of-app>'));
-app.use(express.static(__dirname + '/dist/surcreditosapp'));
+// app.use(express.static(__dirname + '/dist/surcreditosapp'));
 
-app.get('/*', function(req, res) {
-
+/* app.get('/*', function(req, res) {
     // res.sendFile(path.join(__dirname + '/dist/index.html'));
     res.sendFile(path.join(__dirname + '/dist/surcreditosapp/index.html'));
-});
+}); */
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

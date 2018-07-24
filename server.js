d3aeb9edd53284a3af5a0121e5bae07f9885459a
Jorge Mendoza <jormendoza@gmail.com>
2018-07-24 18:27:49 -0300

    cambio de url del index

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/dist/surcreditosapp'));
 app.get('/*', function(req, res) {
 
     // res.sendFile(path.join(__dirname + '/dist/index.html'));
-    res.sendFile(path.join(__dirname + '/dist/surcreditosapp/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/surcreditosapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

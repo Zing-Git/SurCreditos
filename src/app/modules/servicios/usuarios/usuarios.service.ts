f66bc5f4eea99c3100fb852a290d48813c20bef9
Jorge Mendoza <jormendoza@gmail.com>
2018-09-06 18:20:49 -0300

    harcodeado el tipoDNI para crear usuario y el menu para mostrar los roles, salia INDEFINIDO

--- src/app/modules/servicios/usuarios/usuarios.service.ts
+++ src/app/modules/servicios/usuarios/usuarios.service.ts
@@ -38,10 +38,6 @@ export class UsuariosService {
   // public urlBase = 'https://ws-sur-creditos.herokuapp.com';
   public urlBase = 'http://18.223.146.82:3001';
 
-/*   public postNuevaPersonaUrl = this.urlBase + '/nueva_persona';
-  public postNuevaDomicilioUrl = this.urlBase + '/log/nuevo';
-  public postNuevoContactoUrl = this.urlBase + '/nuevo_contacto'; */
-
   // GET URLs
   public urlPostGetPersonaPorDni = this.urlBase + '/persona/obtener_persona/';
   public urlGetTiposDni = this.urlBase + '/persona/tipos_dni';
